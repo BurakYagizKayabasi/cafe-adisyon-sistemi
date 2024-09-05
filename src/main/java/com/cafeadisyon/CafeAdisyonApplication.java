427d0938a001aefd484a58b798813f9021840be0
BurakYagizKayabasi <burakygz46@gmail.com>
2024-09-05 13:25:29 +0300

    adisyon projesi oluşturuldu.

---
+++ src/main/java/com/cafeadisyon/CafeAdisyonApplication.java
@@ -0,0 +1,14 @@
+package com.cafeadisyon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeAdisyonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeAdisyonApplication.class, args);
+        System.out.println("Cafe Adisyon Application Started");
+    }
+
+}
---
+++ src/test/java/com/cafeadisyon/CafeAdisyonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafeadisyon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeAdisyonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
